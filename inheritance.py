c0e32bca0734c6c0c71cc1a8b3d5bd77dc0502bc
tapan-07 <60962421+tapan-07@users.noreply.github.com>
2020-02-12 15:30:18 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+
+x = Person("SANJU", "XYZ")
+x.printname()
